ed4de366c5fda00b889b2d56a99af1f636dbe9d0
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-27 19:41:44 +0530

    title style and rating added

--- src/components/SingleContent.js
+++ src/components/SingleContent.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import { img300, unavailable } from "../config/config";
 
+import Badge from '@material-ui/core/Badge';
+
 const SingleContent = ({ id, poster, title, date, media_type, vote_average}) => {
+
+    const voteColor = (voteAvg) => {
+        if(voteAvg > 7){
+            return "primary"
+        }
+        else if(voteAvg < 7 && voteAvg > 4){
+            return "secondary"
+        }
+        else {
+            return "error"
+        }
+    }
+
     return (
         <div className="media">
+            <Badge 
+                badgeContent={vote_average}
+                color={voteColor(vote_average)}>
+            </Badge>
             <img 
                 src={ poster ? `${img300}${poster}` : unavailable} 
                 alt={title} 
