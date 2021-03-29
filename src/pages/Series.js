c39fdd9d4857bc2a17fbdb63b5dee70d1a48c20a
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-29 19:31:31 +0530

    fetching series

--- src/pages/Series.js
+++ src/pages/Series.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import SingleContent from '../components/SingleContent'
+import CustomPagination from '../components/CustomPagination'
+import Genres from '../components/Genres'
+import useGenre from '../hooks/useGenre'
+
+const movieURL = 'https://api.themoviedb.org/3/discover/tv?'
+const apiKey = `api_key=${process.env.REACT_APP_API_KEY}`
 
 const Series = () => {
+
+    const [page, setPage] = useState(1)
+    const [movies, setMovies] = useState([])
+    const [numOfPages, setNumOfPages] = useState()
+
+    const [genres, setGenres] = useState([])
+    const [selectedGenres, setSelectedGenres] = useState([])
+
+    const genreForURL = useGenre(selectedGenres)
+
+    const fetchMovies = async () => {
+        const {data} = await axios.get(`${movieURL}${apiKey}&page=${page}&with_genres=${genreForURL}`)
+
+        // console.log(data)
+
+        setMovies(data.results)
+        setNumOfPages(data.total_pages)
+    }
+
+    useEffect(() => {
+        fetchMovies()
+        // eslint-disable-next-line
+    }, [page, genreForURL])
+
     return (
         <div>
-            <span className="page-title">Series</span>
+            <span className="page-title">TV Series</span>
+            <Genres 
+                type="movie"
+                genres={genres} 
+                setGenres={setGenres}
+                selectedGenres={selectedGenres} 
+                setSelectedGenres={setSelectedGenres}
+                setPage={setPage}
+            />
+            <div className="trending">
+                {movies && movies.map(movie => (
+                    <SingleContent 
+                        key={movie.id} 
+                        id={movie.id} 
+                        poster={movie.poster_path} 
+                        title={movie.title || movie.name} 
+                        date={movie.release_date || movie.first_air_date} 
+                        media_type="tv"
+                        vote_average={movie.vote_average}
+                    />
+                ))}
+            </div>
+            {numOfPages > 1 && 
+                <CustomPagination 
+                setPage={setPage} 
+                numOfPages={numOfPages} 
+            />}
         </div>
     )
 }
