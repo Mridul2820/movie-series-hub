8b6762c0b038df6a104984ae044bb28982c46f30
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-30 12:11:11 +0530

    the youtube iframe issues fix

--- src/components/ContentModal.js
+++ src/components/ContentModal.js
@@ -134,9 +134,9 @@ const ContentModal = ({ children, media_type, id }) => {
                                     height="315" 
                                     src={`https://www.youtube.com/embed/${video}`}
                                     title="YouTube video player" 
-                                    frameborder="0" 
+                                    frameBorder="0" 
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                                    allowfullscreen
+                                    allowFullScreen
                                 >
                                 </iframe>
                             </div>
